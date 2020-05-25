ac224d3d897557ea615f9e6d58b4548ea475be37
tanishka2708 <54432754+tanishka2708@users.noreply.github.com>
2020-05-25 14:07:54 +0530

    Add files via upload

---
+++ hello.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+<?php echo "<h1>HELLO world!!!</h1>"; ?>
+  </body>
+</html>
